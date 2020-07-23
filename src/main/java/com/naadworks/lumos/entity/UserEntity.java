ab91ea6681a8445bdf9747527912e11deeffa093
Nancy Nigam <nancynigam94@gmail.com>
2020-07-23 23:08:55 +0530

    initial commit for lumos

---
+++ src/main/java/com/naadworks/lumos/entity/UserEntity.java
@@ -0,0 +1,77 @@
+package com.naadworks.lumos.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue()
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @Column(name = "country")
+    private String country;
+
+    public UserEntity(Long id) {
+        this.id = id;
+    }
+
+    public UserEntity() {
+    }
+
+    public UserEntity(Long id, String name, Integer age, String country) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntry{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/naadworks/lumos/exception/DaoException.java
@@ -0,0 +1,11 @@
+package com.naadworks.lumos.exception;
+
+public class DaoException extends RuntimeException {
+
+    public DaoException() {
+    }
+
+    public DaoException(String message) {
+        super(message);
+    }
+}
